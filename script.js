51f973e7e1c6a6b82d79a00207821012f5deccdd
Payton <paytonsweet@usf.edu>
2025-01-23 14:48:20 -0500

    Task 1 - Employee Information

---
+++ script.js
@@ -0,0 +1,11 @@
+// Task 1: Variable Declaration
+
+let employeeName = "Jane Smith";
+console.log(typeof employeeName);
+
+const employeeID = 1234;
+console.log(typeof employeeID); 
+
+var isActive = true;
+console.log(typeof isActive);
+
